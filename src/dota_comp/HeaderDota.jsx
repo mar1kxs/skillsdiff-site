cec6345601b2dc2895bb45fed8e97e7c824460d4
mar1kxs <maksim234.kyky@gmail.com>
2025-12-22 23:53:18 +0100

    d2fix

--- src/dota_comp/HeaderDota.jsx
+++ src/dota_comp/HeaderDota.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/dotahdr.css";
 import icon1 from "../assets/d2featicon.svg";
+import dotaBg from "../assets/DotaBG.png";
 
 const HeaderDota = () => {
   return (
     <header className="dota-hdr">
-      <img src="../src/assets/DotaBG.png" alt="Dota BG" className="dota-bg" />
+      <img src={dotaBg} alt="Dota BG" className="dota-bg" />
+
       <div className="dota-main-div">
         <ul className="dota-hdr-ul">
           <li>Анализ игры</li>
@@ -13,39 +15,45 @@ const HeaderDota = () => {
           <li>Персональные тренировки</li>
           <li>Party-игры с тренером</li>
         </ul>
+
         <h2>
           Путь к победе <br /> начинается с{" "}
           <span style={{ color: "#FF6C32" }}>тренировки</span>
         </h2>
-        <button>Начать тренировку сейчас!</button>
+
+        <button type="button">Начать тренировку сейчас!</button>
       </div>
+
       <div className="dota-features">
         <ul>
           <li>
             <div>
-              <img src={icon1} alt="" />
-            </div>{" "}
+              <img src={icon1} alt="feature icon" />
+            </div>
             <p>Результаты гарантированы</p>
           </li>
+
           <li>
             <div>
-              <img src={icon1} alt="" />{" "}
+              <img src={icon1} alt="feature icon" />
             </div>
             <p>
               Безусловный <br />
               успех
             </p>
           </li>
+
           <li>
             <div>
-              <img src={icon1} alt="" />{" "}
-            </div>{" "}
-            <p>Команда профи- тренеров</p>
+              <img src={icon1} alt="feature icon" />
+            </div>
+            <p>Команда профи-тренеров</p>
           </li>
+
           <li>
             <div>
-              <img src={icon1} alt="" />{" "}
-            </div>{" "}
+              <img src={icon1} alt="feature icon" />
+            </div>
             <p>Гибкость и удобство</p>
           </li>
         </ul>
