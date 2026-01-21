30525b08b49f5fbde08f2ee8c85742227480dd40
mar1kxs <maksim234.kyky@gmail.com>
2026-01-21 19:08:33 +0100

    games fix

--- src/main_comp/Coaches.jsx
+++ src/main_comp/Coaches.jsx
@@ -117,7 +117,7 @@ const Coaches = (props) => {
                           </span>
                           {part}
                         </span>
-                      )
+                      ),
                     )}
                   </p>
 
@@ -135,7 +135,7 @@ const Coaches = (props) => {
                             </span>
                             {part}
                           </span>
-                        )
+                        ),
                       )}
                   </p>
                 </div>
